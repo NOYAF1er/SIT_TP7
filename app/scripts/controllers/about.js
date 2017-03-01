db2ccb11607ce1c8dc470fffadb32c91244575ee
NOYAF1er <yannickossuih@gmail.com>
2017-03-01 15:34:47 +0100

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sirTp7App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sirTp7App
+ */
+angular.module('sirTp7App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
